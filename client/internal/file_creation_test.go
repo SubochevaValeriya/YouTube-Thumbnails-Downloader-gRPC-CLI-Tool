acf6b0c7a50499267eb7782e04c887081417ab57
Valeriya Subocheva <subocheva.valeria@gmail.com>
2023-02-01 22:36:55 +0300

    test corrected

--- client/internal/file_creation_test.go
+++ client/internal/file_creation_test.go
@@ -8,18 +8,6 @@ import (
 	"testing"
 )
 
-func TestCreateFolder(t *testing.T) {
-	err := CreateFolder()
-	if err != nil {
-		t.Fatal()
-	}
-
-	if _, err := os.Stat("thumbnails"); os.IsNotExist(err) {
-		fmt.Println(err)
-		t.Fatal()
-	}
-}
-
 func TestSaveThumbnail(t *testing.T) {
 	resp, err := http.Get("https://i.ytimg.com/vi/6o1m7ofjCCY/hqdefault.jpg")
 	if err != nil {
